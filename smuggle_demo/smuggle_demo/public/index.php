d611706d5c53f0f937df1ea0da11b2c827b7de87
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-10-12 17:45:08 +0530

    demo vulnerable app added

---
+++ smuggle_demo/smuggle_demo/public/index.php
@@ -0,0 +1,14 @@
+<?php
+$title = "Welcome";
+include_once("_header.php");
+?>
+
+<h2>“Nobody exists on purpose. Nobody belongs anywhere. We’re all going to die. Come watch TV.”</h2>
+<p>
+"Listen, Morty, I hate to break it to you, but what people call “love” is just a chemical reaction that compels animals to breed. It hits hard, Morty, then it slowly fades, leaving you stranded in a failing marriage. I did it. Your parents are gonna do it. Break the cycle, Morty. Rise above. Focus on science."
+</p>
+
+<?php
+include_once("_footer.php");
+?>
+
